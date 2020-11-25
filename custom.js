cb84e882b9cae261313005bbf0de41b2086f96ab
jfmverissimo <54084438+jfmverissimo@users.noreply.github.com>
2020-11-25 22:48:02 +0100

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,46 @@
+function adjustWidth() {
+    var parentwidth = $(".asideContent").width();
+    $(".propertyContactForm").width(parentwidth);
+}
+
+$(document).ready(function () {
+
+
+    $(window).resize(
+        function () {
+            adjustWidth();
+        });
+
+    $(window).scroll(
+        function () {
+            var mBool = true;
+            if ($(window).innerWidth() > 768) {
+                var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+                var HImos = $('.propertyInfo').height() - ($('.propertyContactForm').height() - 30);
+
+                /*imos */
+                if (mBool) {
+                    if ($(".PropertyBasicDetail").length) {
+                        if (scrollTop > 180) {
+                            $('.Element.PropertyBasicDetail .propertyContactForm').addClass('fixA');
+                        } else {
+                            $('.Element.PropertyBasicDetail .propertyContactForm').removeClass('fixA');
+                        }
+                        if (scrollTop > HImos) {
+                            $('.Element.PropertyBasicDetail .asideContent').addClass('fixB');
+                            mBool = false;
+                            $('.Element.PropertyBasicDetail .propertyContactForm').removeClass('fixA');
+                        } else {
+                            $('.Element.PropertyBasicDetail .asideContent').removeClass('fixB');
+                        }
+                    }
+                }
+                /**/
+                if (scrollTop < HImos) {
+                    mBool = true;
+                    $('.Element.PropertyBasicDetail .asideContent').removeClass('fixB');
+                }
+            }
+        });
+});
+
